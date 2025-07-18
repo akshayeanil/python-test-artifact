f56252ff5c1976a6a07c3cbe0cda69e68d4e1563
akshayeanil <akshayanil770@gmail.com>
2025-07-18 18:39:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello from Flask!'
-
-def add(a, b):
-    return a + b
+def hello():
+    return 'Hello, GitHub Actions with Docker!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
