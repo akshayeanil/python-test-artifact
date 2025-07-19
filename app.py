cc1e827b4ba7bcc426518eb4dbc14a4cfb314e0a
akshayeanil <akshayanil770@gmail.com>
2025-07-18 20:39:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return jsonify(message="Hello from Flask app!")
+@app.route('/')
+def home():
+    return "Hello from Flask!"
+
+def add(a, b):
+    return a + b
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
+    # Run the Flask app on all network interfaces on port 5000
+    app.run(host="0.0.0.0", port=5000, debug=True)
