a83817fb09d0b696e41d05709db5eee54c4a64df
akshayeanil <akshayanil770@gmail.com>
2025-07-18 20:33:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello from Flask!'
+@app.route("/")
+def hello():
+    return jsonify(message="Hello from Flask app!")
 
-if __name__ == '__main__':
-    # Run Flask on all interfaces, no debug, no reloader
-    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
